b11a1da30af22db95f54af989b1b1dad5f0ae6a5
Somkiet <somsor@perfenterprise.com>
2018-09-01 14:24:54 +0700

    inject http

--- EZ.Lunch.Mobile/src/app/app.module.ts
+++ EZ.Lunch.Mobile/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { OrderHistoryPage } from '../pages/order-history/order-history';
 import { ShopCreatePage } from '../pages/shop-create/shop-create';
 import { ShopDetailPage } from '../pages/shop-detail/shop-detail';
 import { ShopManagePage } from '../pages/shop-manage/shop-manage';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { ShopManagePage } from '../pages/shop-manage/shop-manage';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
@@ -46,6 +48,7 @@ import { ShopManagePage } from '../pages/shop-manage/shop-manage';
   providers: [
     StatusBar,
     SplashScreen,
+    HttpClient,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
