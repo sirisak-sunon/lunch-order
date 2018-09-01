750363f5d14e16358d274f912d9d6a3e5cb41ad3
Somkiet <somsor@perfenterprise.com>
2018-09-01 11:28:24 +0700

    inject controller

--- EZ.Lunch.Api/EZ.Lunch.Api/Controllers/PollController.cs
+++ EZ.Lunch.Api/EZ.Lunch.Api/Controllers/PollController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EZ.Lunch.Api.Repositories;
+using EZ.Lunch.Api.Repositories.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,8 +12,23 @@ namespace EZ.Lunch.Api.Controllers
     [Route("api/[controller]/[action]")]
     public class PollController : Controller
     {
+        public readonly IDataRepository<Poll> PollDac;
+        public readonly IDataRepository<Shop> ShopDac;
+        public readonly IDataRepository<User> UserDac;
+
+        public PollController(
+            IDataRepository<Poll> PollDac,
+            IDataRepository<Shop> ShopDac,
+            IDataRepository<User> UserDac
+            )
+        {
+            this.PollDac = PollDac;
+            this.ShopDac = ShopDac;
+            this.UserDac = UserDac;
+        }
+
         [HttpGet]
-        public void ShowPoll ()
+        public void ShowPoll()
         {
             throw new NotImplementedException();
         }
--- EZ.Lunch.Api/EZ.Lunch.Api/Controllers/ShopController.cs
+++ EZ.Lunch.Api/EZ.Lunch.Api/Controllers/ShopController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EZ.Lunch.Api.Repositories;
+using EZ.Lunch.Api.Repositories.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +12,21 @@ namespace EZ.Lunch.Api.Controllers
     [Route("api/[controller]/[action]")]
     public class ShopController : Controller
     {
+        public readonly IDataRepository<Poll> PollDac;
+        public readonly IDataRepository<Shop> ShopDac;
+        public readonly IDataRepository<User> UserDac;
+
+        public ShopController(
+            IDataRepository<Poll> PollDac,
+            IDataRepository<Shop> ShopDac,
+            IDataRepository<User> UserDac
+            )
+        {
+            this.PollDac = PollDac;
+            this.ShopDac = ShopDac;
+            this.UserDac = UserDac;
+        }
+
         public ShopController()
         {
         }
